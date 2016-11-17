6f50909451b8731a3aea8a6e7f02601d043a7e85
unaitxutxu <unaialaezdearaluze@hotmail.com>
2016-11-17 09:48:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>conectando con gmail</title>
+<link href="estilos.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<form name="sincronizar" method="post" action="index?variable=OK.php">
+¿desea acceder a sus contactos de Gmail?<br>
+<input type="radio" name="sinc" value="si"> SI<br>
+<input type="radio" name="sinc" value="no"> NO<br>
+<input type="submit" value ="enviar">
+</form>
+</body>
+</html>
